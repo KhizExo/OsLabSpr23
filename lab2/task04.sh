b842a4153c3b238e39a4ee996bf2f1c4f97d3b37
KhizExo <123970961+KhizExo@users.noreply.github.com>
2023-02-07 21:32:08 -0600

    Update task04.sh

--- lab2/task04.sh
+++ lab2/task04.sh
@@ -3,10 +3,6 @@
 # Create an empty fileNames.txt file
 touch fileNames.txt
 
-# Search for all text files in the current folder and its subdirectories
-# and write their names to fileNames.txt
 find . -name "*.txt" -type f -print >> fileNames.txt
-
-# Print a message indicating the script has finished
 echo "Finished writing names of text files to fileNames.txt"
 
